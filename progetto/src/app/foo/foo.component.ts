538938653923a6c9b4298b3c5b83632b2cbcb8f9
ahmed <159021809+crazymidoo@users.noreply.github.com>
2025-05-12 07:45:02 +0000

    Finito

--- progetto/src/app/foo/foo.component.ts
+++ progetto/src/app/foo/foo.component.ts
@@ -1,46 +1,59 @@
-import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Foo } from '../model/foo.model';
 import { Observable } from 'rxjs';
 import { JsonPipe } from '@angular/common';
 
 @Component({
- selector: 'app-foo',
- standalone: true,
- imports: [JsonPipe],
- templateUrl: './foo.component.html',
- styleUrls: ['./foo.component.css']
+  selector: 'app-foo',
+  standalone: true,
+  imports: [JsonPipe],
+  templateUrl: './foo.component.html',
+  styleUrls: ['./foo.component.css']
 })
 export class FooComponent {
-   data!: Object; 
-   loading: boolean=false;
-   o! :Observable<Object>;
-   constructor(public http: HttpClient) {}
-   makeRequest(): void {
-     console.log("here");
-     this.loading = true;
-     this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
-     this.o.subscribe(this.getData);
-   }
-   getData = (d : Object) =>
-   {
-     this.data = new Object(d);
-     this.loading = false;
-   }
+  fooData! : Foo[];
+  data!: Object;
+  loading: boolean = false;
+  o!: Observable<Object>; // oggetto che attende i dati - o porta la pizza
+  o2!: Observable<Object>;
+  oFoo! : Observable<Foo[]>;
 
-   makeCompactPost(): void {
+  constructor(public http: HttpClient) { } //passare oggetto httpclient
+
+  makeRequest(): void {
+    console.log("here");
     this.loading = true;
-    this.http
-      .post('https://jsonplaceholder.typicode.com/posts',
-        JSON.stringify({
-          body: 'bar',
-          title: 'foo',
-          userId: 1
-        })
-      )
-      .subscribe(data => { 
-        this.data = data;
-        this.loading = false;
-      });
+    this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1'); // chiamata get http
+    this.o.subscribe(this.getData);
   }
- 
+  getData = (d: Object) => {
+    this.data = new Object(d); // piatto data - pizza d
+    this.loading = false;
   }
+
+
+  makePost(): void {
+    this.loading = true;
+    let datiDaInviare = JSON.stringify({
+      body: 'bar',
+      title: 'foo',
+      userId: 1
+    });
+    this.o2 = this.http.post('https://jsonplaceholder.typicode.com/posts', datiDaInviare);
+    this.o2.subscribe(this.rispostaPost);
+  }
+
+  rispostaPost = (data : any) => {
+    this.data = data;
+    this.loading = false;
+  }
+
+  makeTypedRequest() : void
+  {
+    //oFoo : Observable<Foo[]>; Observable di tipo Foo[] è stao dichiarato tra gli attributi della classe 
+    this.oFoo = this.http.get<Foo[]>('https://jsonplaceholder.typicode.com/posts');
+    this.oFoo.subscribe(data => {this.fooData = data;});
+  }
+
+}
